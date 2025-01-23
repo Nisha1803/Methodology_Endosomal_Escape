6560471bb16a2e8e7af65742ccc8cf3eb30b5a13
Nisha1803 <107213424+Nisha1803@users.noreply.github.com>
2025-01-22 19:05:10 -0500

    Update extract_pixels.py

--- extract_pixels.py
+++ extract_pixels.py
@@ -1,44 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-
-# Separating Channels
-
-
-# In[8]:
-
-
-# !pip install --upgrade opencv-python
-
-
-# In[2]:
-
-
-import nbformat
-from nbconvert import PythonExporter
-
-# Load the notebook
-
-with open('C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\Endosomal_codes\\STEP1_exp_data.ipynb') as f:
-    nb = nbformat.read(f, as_version=4)
-
-# Convert to Python script
-exporter = PythonExporter()
-script, _ = exporter.from_notebook_node(nb)
-
-# Save to .py file
-with open('C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\Endosomal_codes\\STEP1_exp_data.py', 'w') as f:
-    f.write(script)
-
-
-# In[6]:
-
-
-# Display the image
-# blue_channel, green_channel, red_channel = cv2.split(image)
-import os
 import os
 import cv2
 import numpy as np
@@ -46,94 +6,10 @@ import skimage.io
 import skimage.color
 from skimage.filters import threshold_otsu
 import csv
-
 import matplotlib.pyplot as plt
-# plt.imshow(red_channel)
-# plt.show()
-image= skimage.io.imread('C:\\Users\\fnisha\OneDrive - Clemson University\\Desktop\\60\\T0001\\Image_T0001_XY01_Z001_Red and Green Overlay.tif')
-# image = skimage.io.imread('C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\copied_60\\T0001\\Image_T0001_XY01_Z001_Red and Green Overlay.tif')
-# image = skimage.io.imread('C:\\Users\\fnisha\\OneDrive - Clemson University\\Desktop\\T0040 Overlay All Colors.tif')
-red_channel = image[:, :, 0]
-green_channel = image[:, :, 1]
-blue_channel = image[:, :, 2]
+import pandas as pd
+from collections import defaultdict
 
-# Display the original image
-plt.imshow(image)
-plt.xlabel('Original Image')
-plt.show()
-
-# Display the red channel
-plt.imshow(red_channel)
-plt.xlabel('SiRNA')
-plt.show()
-
-# Display the green channel
-plt.imshow(green_channel)
-plt.xlabel('Endosome')
-plt.show()
-
-# # Display the blue channel
-plt.imshow(blue_channel)
-plt.xlabel('Nucleus')
-plt.show()
-
-
-# In[7]:
-
-
-# Display the image
-# blue_channel, green_channel, red_channel = cv2.split(image)
-import os
-import os
-import cv2
-import numpy as np
-import skimage.io
-import skimage.color
-from skimage.filters import threshold_otsu
-import csv
-
-import matplotlib.pyplot as plt
-# plt.imshow(red_channel)
-# plt.show()
-image= skimage.io.imread('C:\\Users\\fnisha\OneDrive - Clemson University\\Desktop\\60\\T0026\\Image_T0026_XY01_Z001_Red and Green Overlay.tif')
-# image = skimage.io.imread('C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\copied_60\\T0001\\Image_T0001_XY01_Z001_Red and Green Overlay.tif')
-# image = skimage.io.imread('C:\\Users\\fnisha\\OneDrive - Clemson University\\Desktop\\T0040 Overlay All Colors.tif')
-red_channel = image[:, :, 0]
-green_channel = image[:, :, 1]
-blue_channel = image[:, :, 2]
-
-# Display the original image
-plt.imshow(image)
-plt.xlabel('Original Image')
-plt.show()
-
-# Display the red channel
-plt.imshow(red_channel)
-plt.xlabel('SiRNA')
-plt.show()
-
-# Display the green channel
-plt.imshow(green_channel)
-plt.xlabel('Endosome')
-plt.show()
-
-# # Display the blue channel
-plt.imshow(blue_channel)
-plt.xlabel('Nucleus')
-plt.show()
-
-
-# # Using Otsu to separate Channels on all of Experimental Data
-
-# In[105]:
-
-
-import os
-import cv2
-import numpy as np
-import skimage.io
-from skimage.filters import threshold_otsu
-import csv
 
 def count_special_red_pixels(image):
     # Ensure the image was provided
@@ -184,12 +60,7 @@ def count_special_red_pixels(image):
 
 # # Converting image data to csv data
 
-# In[108]:
 
-
-import os
-import csv
-import skimage.io
 
 def read_images_from_folders(root_directory):
     # Fixed the path to point to a file rather than a directory
@@ -242,18 +113,10 @@ def read_images_from_folders(root_directory):
                     print(f"Total Red Count: {red_total}")
                     print("----------")
                  
-
-# Example usage
-# root_directory = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1'
-# root_directory = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\image_analysis_ovary\\40'
 root_directory = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\image_analysis_ovary\\80'
 read_images_from_folders(root_directory)
 
 
-# In[109]:
-
-
-import pandas as pd
 
 # Load the CSV file with raw strings
 df = pd.read_csv(r'C:\Users\fnisha\Box\AB-Cook-Nisha Research\image_analysis_ovary\80_raw_edit_endosomal_values_with_ratio.csv')
@@ -275,11 +138,8 @@ print(f"Updated file saved to: {updated_file_with_ratio_path}")
 
 # # Calculating Experimental endosomal escape
 
-# In[111]:
 
 
-import csv
-from collections import defaultdict
 
 def calculate_endosomal_escape(csv_file):
     try:
@@ -355,16 +215,8 @@ csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\image_analysis_
 calculate_endosomal_escape(csv_file_path)
 
 
-# 
-
 # # Plot Experimental Data
 
-# In[1]:
-
-
-import os
-import csv
-import matplotlib.pyplot as plt
 
 def plot_endosomal_escape(csv_file):
     # Read the CSV file
@@ -382,18 +234,12 @@ def plot_endosomal_escape(csv_file):
     rows_801 = [row for row in numeric_rows if row[0] == '80_background_edit']
     rows_copied_80 = [row for row in numeric_rows if row[0] == 'copied_80']
     rows_interpolate_80 = [row for row in numeric_rows if row[0] == 'interpolate_80']
-    # rows_copied_40 = [row for row in numeric_rows if row[0] == 'copied_40']
-    # rows_copied_60 = [row for row in numeric_rows if row[0] == 'copied_60']
-    # rows_copied_20 = [row for row in numeric_rows if row[0] == 'copied_20']
     
     # Extract endosomal escape values from the filtered rows
     endosomal_escape_40 = [float(row[-2]) for row in rows_40]
     endosomal_escape_80 = [float(row[-2]) for row in rows_80]
     endosomal_escape_801 = [float(row[-2]) for row in rows_801]
     endosomal_escape_802 = [float(row[-2]) for row in rows_interpolate_80]
-    # endosomal_escape_copied_20 = [float(row[-2]) for row in rows_copied_20]
-    # endosomal_escape_copied_40 = [float(row[-2]) for row in rows_copied_40]
-    # endosomal_escape_copied_60 = [float(row[-2]) for row in rows_copied_60]
     endosomal_escape_copied_80 = [float(row[-2]) for row in rows_copied_80]
     
     # Define time points for plotting
@@ -412,17 +258,6 @@ def plot_endosomal_escape(csv_file):
     start_time_802 = 45  # Start time in minutes for 80.0 ratio
     time_interval_802 = 5  # Time interval in minutes for 80.0 ratio
     time_points_802 = [(start_time_802 + i * time_interval_802)/60 for i in range(len(endosomal_escape_802))]
-    # start_time_copied_20 = 60  # Start time in minutes for 40.0 ratio
-    # time_interval_copied_20 = 15  # Time interval in minutes for 40.0 ratio
-    # time_points_copied_20 = [(start_time_copied_20 + i * time_interval_copied_20)/60 for i in range(len(endosomal_escape_copied_20))]
-    
-    # start_time_copied_40 = 90  # Start time in minutes for 40.0 ratio
-    # time_interval_copied_40 = 7  # Time interval in minutes for 40.0 ratio
-    # time_points_copied_40 = [(start_time_copied_40 + i * time_interval_copied_40)/60 for i in range(len(endosomal_escape_copied_40))]
-    
-    # start_time_copied_60 = 60  # Start time in minutes for 40.0 ratio
-    # time_interval_copied_60 = 15  # Time interval in minutes for 40.0 ratio
-    # time_points_copied_60 = [(start_time_copied_60 + i * time_interval_copied_60)/60 for i in range(len(endosomal_escape_copied_60))]
     
     start_time_copied_80 = 45  # Start time in minutes for 80.0 ratio
     time_interval_copied_80 = 5  # Time interval in minutes for 80.0 ratio
@@ -430,13 +265,7 @@ def plot_endosomal_escape(csv_file):
     
     # Plot endosomal escape versus time for both main folders
     plt.plot(time_points_40, endosomal_escape_40, marker='o', linestyle='-', label='Partially edited by model')
-    # plt.plot(time_points_40, endosomal_escape_40, marker='o', linestyle='-', label='40_raw.0')
-    # plt.plot(time_points_80, endosomal_escape_80, marker='o', linestyle='-', label='80_mask_raw')
-    # plt.plot(time_points_801, endosomal_escape_801, marker='o', linestyle='-', label='80_background_edit_mask')
     plt.plot(time_points_802, endosomal_escape_802, marker='o', linestyle='-', label='Fully edited by model')
-    # plt.plot(time_points_copied_20, endosomal_escape_copied_20, marker='o', linestyle='-', label='20:1')
-    # plt.plot(time_points_copied_40, endosomal_escape_copied_40, marker='o', linestyle='-', label='40:1')
-    # plt.plot(time_points_copied_60, endosomal_escape_copied_60, marker='o', linestyle='-', label='60:1')
     plt.plot(time_points_copied_80, endosomal_escape_copied_80, marker='o', linestyle='-', label='Fully edited by hand')
     
     plt.xlabel('Time (hours)')
@@ -447,25 +276,11 @@ def plot_endosomal_escape(csv_file):
     plt.grid(True)
     plt.show()
 
-# Example usage
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\endosomal_values.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\copied_80_endosomal_values.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\updated_endosomal_values_with_ratio.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\image_analysis_ovary\\80_raw_edit_endosomal_values_with_ratio.csv'
 csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\image_analysis_ovary\\80_dataset_with_fixed_cycles.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\All_RATIO_endosomal_values.csv'
+
 plot_endosomal_escape(csv_file_path)
 
 
-
-
-
-# In[57]:
-
-
-import os
-import csv
-import matplotlib.pyplot as plt
 
 def plot_endosomal_escape(csv_file):
     # Read the CSV file
@@ -482,10 +297,7 @@ def plot_endosomal_escape(csv_file):
     rows_80 = [row for row in numeric_rows if row[0] == '80_raw_image_output_folder']
     rows_801 = [row for row in numeric_rows if row[0] == '80_background_edit']
     rows_copied_80 = [row for row in numeric_rows if row[0] == 'copied_80']
-   
-    # rows_copied_40 = [row for row in numeric_rows if row[0] == 'copied_40']
-    # rows_copied_60 = [row for row in numeric_rows if row[0] == 'copied_60']
-    # rows_copied_20 = [row for row in numeric_rows if row[0] == 'copied_20']
+
     
     # Extract endosomal escape values from the filtered rows
     endosomal_escape_40 = [float(row[-2]) for row in rows_40]
@@ -508,17 +320,7 @@ def plot_endosomal_escape(csv_file):
     start_time_801 = 45  # Start time in minutes for 80.0 ratio
     time_interval_801 = 5  # Time interval in minutes for 80.0 ratio
     time_points_801 = [(start_time_801 + i * time_interval_801)/60 for i in range(len(endosomal_escape_801))]
-    # start_time_copied_20 = 60  # Start time in minutes for 40.0 ratio
-    # time_interval_copied_20 = 15  # Time interval in minutes for 40.0 ratio
-    # time_points_copied_20 = [(start_time_copied_20 + i * time_interval_copied_20)/60 for i in range(len(endosomal_escape_copied_20))]
-    
-    # start_time_copied_40 = 90  # Start time in minutes for 40.0 ratio
-    # time_interval_copied_40 = 7  # Time interval in minutes for 40.0 ratio
-    # time_points_copied_40 = [(start_time_copied_40 + i * time_interval_copied_40)/60 for i in range(len(endosomal_escape_copied_40))]
-    
-    # start_time_copied_60 = 60  # Start time in minutes for 40.0 ratio
-    # time_interval_copied_60 = 15  # Time interval in minutes for 40.0 ratio
-    # time_points_copied_60 = [(start_time_copied_60 + i * time_interval_copied_60)/60 for i in range(len(endosomal_escape_copied_60))]
+
     
     start_time_copied_80 = 45  # Start time in minutes for 80.0 ratio
     time_interval_copied_80 = 5  # Time interval in minutes for 80.0 ratio
@@ -526,12 +328,7 @@ def plot_endosomal_escape(csv_file):
     
     # Plot endosomal escape versus time for both main folders
     plt.plot(time_points_40, endosomal_escape_40, marker='o', linestyle='-', label='80_mask_edit')
-    # plt.plot(time_points_40, endosomal_escape_40, marker='o', linestyle='-', label='40_raw.0')
-    # plt.plot(time_points_80, endosomal_escape_80, marker='o', linestyle='-', label='80_mask_raw')
     plt.plot(time_points_801, endosomal_escape_801, marker='o', linestyle='-', label='80_background_edit_mask')
-    # plt.plot(time_points_copied_20, endosomal_escape_copied_20, marker='o', linestyle='-', label='20:1')
-    # plt.plot(time_points_copied_40, endosomal_escape_copied_40, marker='o', linestyle='-', label='40:1')
-    # plt.plot(time_points_copied_60, endosomal_escape_copied_60, marker='o', linestyle='-', label='60:1')
     plt.plot(time_points_copied_80, endosomal_escape_copied_80, marker='o', linestyle='-', label='hand edit 80:1')
     
     plt.xlabel('Time (hours)')
@@ -542,20 +339,9 @@ def plot_endosomal_escape(csv_file):
     plt.grid(True)
     plt.show()
 
-# Example usage
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\endosomal_values.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\copied_80_endosomal_values.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\updated_endosomal_values_with_ratio.csv'
+
 csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\image_analysis_ovary\\80_raw_edit_endosomal_values_with_ratio.csv'
-# csv_file_path = 'C:\\Users\\fnisha\\Box\\AB-Cook-Nisha Research\\data_ovary\\data_ovary1\\All_RATIO_endosomal_values.csv'
+
 plot_endosomal_escape(csv_file_path)
 
-
-
-
-
-# In[ ]:
-
-
-#interpolate time series data point only for outliers
 
